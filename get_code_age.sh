6fb016c13fee449e175ca2062293789557f0c6cc
detoix <dk14yo@gmail.com>
2021-10-25 18:46:27 +0200

    group info by directories

--- get_code_age.sh
+++ get_code_age.sh
@@ -9,13 +9,25 @@ file_max_timestamp=0
 
 echo 'File' $'\t' 'File length [chars]' $'\t' 'Average last modification [timestamp]' $'\t' 'Last modification [timestamp]'
 
+declare -A timestamp_by_dir
+declare -A chars_by_dir
+
 while read line
 do
     if [[ $line == *"-_-"* ]]
     then
         if [ $file_length != "0" ] && [ $file_timestamp != "0" ]
         then
-            echo $line $'\t' $file_length $'\t' $(( $file_timestamp / $file_length )) $'\t' $file_max_timestamp | sed -z 's/-_-//g'
+            path=`echo "$line" | sed -z 's/-_-//g'`
+
+            echo $path $'\t' $file_length $'\t' $(( $file_timestamp / $file_length )) $'\t' $file_max_timestamp
+            
+            while [[ $path == *"/"* ]]
+            do
+                path=${path%/*}
+                timestamp_by_dir[$path]=$((${timestamp_by_dir[$path]} + $file_timestamp))
+                chars_by_dir[$path]=$((${chars_by_dir[$path]} + $file_length))
+            done
         fi
 
         i=-1
@@ -34,3 +46,8 @@ do
 
     i=$i+1
 done < <(git ls-files *.cs --exclude-standard | xargs -I{} sh -c 'git blame {} --line-porcelain ; echo -_-{}' | sed -n 's/^committer-time //p;s/^\t//p;s/^-_-/-_-/p' | sed 's/\\//g')
+
+for path in "${!timestamp_by_dir[@]}"
+do
+    echo $path $'\t' ${chars_by_dir[$path]} $'\t' $(( ${timestamp_by_dir[$path]} / ${chars_by_dir[$path]} )) $'\t' 0
+done
