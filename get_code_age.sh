c39fbb2bb5bd4b34901f195d076aba733253b849
detoix <dk14yo@gmail.com>
2021-10-26 21:50:34 +0200

    add curly braces count and max timestamp by dir

--- get_code_age.sh
+++ get_code_age.sh
@@ -4,29 +4,40 @@ i=0
 file=0
 timestamp=0
 file_length=0
+file_curly_braces_count=0
 file_timestamp=0
 file_max_timestamp=0
 
-echo 'File' $'\t' 'File length [chars]' $'\t' 'Average last modification [timestamp]' $'\t' 'Last modification [timestamp]'
+echo 'File'$'\t''File length [chars]'$'\t''Average last modification [timestamp]'$'\t''Last modification [timestamp]'$'\t''Leading curly braces [count]'
 
 declare -A timestamp_by_dir
 declare -A chars_by_dir
+declare -A max_timestamp_by_dir
+declare -A curly_braces_by_dir
 
 while read line
 do
     if [[ $line == *"-_-"* ]]
     then
-        if [ $file_length != "0" ] && [ $file_timestamp != "0" ]
+        if [ $file_length != "0" ]
         then
             path=`echo "$line" | sed -z 's/-_-//g'`
 
-            echo $path $'\t' $file_length $'\t' $(( $file_timestamp / $file_length )) $'\t' $file_max_timestamp
+            echo $path$'\t'$file_length$'\t'$(( $file_timestamp / $file_length ))$'\t'$file_max_timestamp$'\t'$file_curly_braces_count
             
             while [[ $path == *"/"* ]]
             do
                 path=${path%/*}
                 timestamp_by_dir[$path]=$((${timestamp_by_dir[$path]} + $file_timestamp))
                 chars_by_dir[$path]=$((${chars_by_dir[$path]} + $file_length))
+                curly_braces_by_dir[$path]=$((${curly_braces_by_dir[$path]} + $file_curly_braces_count))
+
+                if [ -z "${max_timestamp_by_dir[$path]}" ]
+                then
+                    max_timestamp_by_dir[$path]=0
+                fi
+
+                max_timestamp_by_dir[$path]=$(( ${max_timestamp_by_dir[$path]} > $file_max_timestamp ? ${max_timestamp_by_dir[$path]} : $file_max_timestamp ))
             done
         fi
 
@@ -34,6 +45,7 @@ do
         file_length=0
         file_timestamp=0
         file_max_timestamp=0
+        file_curly_braces_count=0
     elif (( i % 2 == 0 ))
     then
         timestamp="$line"
@@ -42,6 +54,11 @@ do
     then
         file_length=$(( $file_length + ${#line} ))
         file_timestamp=$(( $file_timestamp + $timestamp * ${#line} ))
+
+        if [[ `echo "$line" | tr -d ' '` == "{" ]]
+        then
+            file_curly_braces_count=$(( $file_curly_braces_count + 1 ))
+        fi
     fi
 
     i=$i+1
@@ -49,5 +66,7 @@ done < <(git ls-files *.cs --exclude-standard | xargs -I{} sh -c 'git blame {} -
 
 for path in "${!timestamp_by_dir[@]}"
 do
-    echo $path $'\t' ${chars_by_dir[$path]} $'\t' $(( ${timestamp_by_dir[$path]} / ${chars_by_dir[$path]} )) $'\t' 0
+    echo $path$'\t'${chars_by_dir[$path]}$'\t'$(( ${timestamp_by_dir[$path]} / ${chars_by_dir[$path]} ))$'\t'${max_timestamp_by_dir[$path]}$'\t'${curly_braces_by_dir[$path]}
 done
+
+#git log --format=format: --name-only | egrep -v '^$' | sort | uniq -c | sort -r | head -5
