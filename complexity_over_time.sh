c76554ccbd6a3e888bc323a3cde26e4f755ff94b
detoix <dk14yo@gmail.com>
2021-10-28 07:23:15 +0200

    change date format and reverse log

--- complexity_over_time.sh
+++ complexity_over_time.sh
@@ -2,7 +2,7 @@
 
 i=0
 
-echo 'Indentation spaces'$'\t''Commit age'
+echo 'File'$'\t''File length [chars]'$'\t''Average last modification [timestamp]'$'\t''Last modification [timestamp]'$'\t''Leading curly braces [count]'
 
 while read line
 do
@@ -10,10 +10,10 @@ do
     then
         $(git checkout --quiet $line)
 
-        echo $(git ls-files *.cs --exclude-standard | xargs -I{} grep -o '^[[:blank:]]' {} | wc -c)$'\t'$(git log -1 --format=%cr)
+        echo $(git ls-files *.cs --exclude-standard | xargs -I{} grep -o '^[[:blank:]]' {} | wc -c)$'\t'$(git log -1 --format=%cs)
     fi
 
     i=$i+1
-done < <(git rev-list HEAD)
+done < <(git rev-list --reverse HEAD)
 
 $(git checkout --quiet master)
