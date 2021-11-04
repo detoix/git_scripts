adc2c72a49d0ed25a9b6149264dfe947e4b9e79f
detoix <dk14yo@gmail.com>
2021-11-04 18:26:43 +0100

    improve scripts

--- complexity_over_time.sh
+++ complexity_over_time.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
+analyze_every_nth_commit=100
+browse_commit_since=1900-01-01
+max_number_of_commits=-1
+filter_files='*.cs'
+
+while getopts e:s:m:f: flag
+do
+    case "${flag}" in
+        e) analyze_every_nth_commit=${OPTARG};;
+        s) browse_commit_since=${OPTARG};;
+        m) max_number_of_commits=${OPTARG};;
+        f) filter_files=${OPTARG};;
+    esac
+done
+
+echo 'Date' 'Commit_hash' 'Leading_spaces' 'Lines_count'
+
 i=0
-
-echo 'Date'$'\t''Leading_spaces'$'\t''Lines_count'$'\t''Total_chars'
-
 while read line
 do
-    if (( i % 20 == 0 ))
+    if (( i % $analyze_every_nth_commit == 0 ))
     then
-        $(git checkout --quiet $line)
+        $(git checkout -f --quiet $line)
 
-        files=$(echo git ls-files *.cs)
+        files=$(echo git ls-files $filter_files)
 
         echo \
-$(git log -1 --format=%cs)\
- \
-$($files | xargs -I{} grep -o '^[[:blank:]]*' {} | wc -c)\
- \
-$($files | xargs -I{} cat {} | wc -ml)
+$(git log -1 --format='%cs %h') \
+$($files | xargs -I{} grep -o '^[[:blank:]]*' {} | wc -c) \
+$($files | xargs -I{} cat {} | wc -l)
 
     fi
 
     i=$i+1
-done < <(git rev-list --reverse HEAD)
+done < <(git rev-list --since=$browse_commit_since --max-count=$max_number_of_commits --reverse HEAD)
 
-$(git checkout --quiet master)
+$(git checkout -f --quiet master)
