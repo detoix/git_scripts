110449f6bae3e2a9d8239239a582b28bcbaed31b
detoix <dk14yo@gmail.com>
2021-12-28 17:52:59 +0100

    improve script

--- find_files_that_change_together.sh
+++ find_files_that_change_together.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-number_of_commits_to_analyze=1000           #number of commits in analyzed git log
-max_files_to_analyze_commit=30              #ignore merges, global renames etc
-min_ratio_to_report_pair=20                 #ignore file pairs that do not change together
-min_combined_commits_to_report_pair=20      #ignore file pairs that rarely change
-min_shared_commits_to_report_pair=5         #ignore file pairs that rarely change
+browse_commit_since=1990-01-01
+max_files_to_analyze_commit=100             #ignore merges, global renames etc
+min_shared_commits_to_report_pair=2         #ignore file pairs that rarely change
 
-while getopts a:f:r:c:s: flag
+while getopts f:m:s: flag
 do
     case "${flag}" in
-        a) number_of_commits_to_analyze=${OPTARG};;
         f) max_files_to_analyze_commit=${OPTARG};;
-        r) min_ratio_to_report_pair=${OPTARG};;
-        c) min_combined_commits_to_report_pair=${OPTARG};;
-        s) min_shared_commits_to_report_pair=${OPTARG};;
+        m) min_shared_commits_to_report_pair=${OPTARG};;
+        s) browse_commit_since=${OPTARG};;
     esac
 done
 
@@ -22,7 +18,7 @@ declare -A file_commits_count
 
 while read commited_files
 do
-    files_in_commit=($(echo $commited_files | tr "," "\n"))
+    files_in_commit=($(echo $commited_files))
     files_in_commit_count=${#files_in_commit[@]}
 
     if (( $files_in_commit_count <= $max_files_to_analyze_commit ))
@@ -30,35 +26,30 @@ do
         for ((i=0;i<$files_in_commit_count;i++))
         do
             file=${files_in_commit[$i]}
+            file_directory="${file%/*}" #jump to parent directory
             file_commits_count[$file]=$((${file_commits_count[$file]} + 1))
             next_i=$i+1 
 
             for ((j=$next_i;j<$files_in_commit_count;j++)) 
             do
                 other_file=${files_in_commit[$j]}
-                file_pair_commits_count[$file,$other_file]=$((${file_pair_commits_count[$file,$other_file]} + 1))
+                other_file_directory="${other_file%/*}" #jump to parent directory
+
+                if [ "$file_directory" != "$other_file_directory" ]
+                then
+                    file_pair_commits_count[$file,$other_file]=$((${file_pair_commits_count[$file,$other_file]} + 1))
+                fi
             done
         done
     fi
-done < <(git log --oneline --name-only --max-count=$number_of_commits_to_analyze --pretty=format:'---' | sed -z 's/---\n//g;s/\n/\t/g;s/\t\t/\n/g')
+done < <(git log --oneline --name-only --since=$browse_commit_since --pretty=format:'' | sed -z 's/\n/\t/g;s/\t\t/\n/g')
 
 for pair in "${!file_pair_commits_count[@]}"
 do
-    combined_commits_to_pair=0
-
-    for file_name in $(echo $pair | tr "," "\n")
-    do
-        combined_commits_to_pair=$((${file_commits_count[$file_name]} + $combined_commits_to_pair))
-    done
-
-    ratio=$((100 * ${file_pair_commits_count["$pair"]} / $combined_commits_to_pair))
     shared_commits=${file_pair_commits_count["$pair"]}
 
-    if (( $ratio >= $min_ratio_to_report_pair )) && (( $combined_commits_to_pair >= $min_combined_commits_to_report_pair )) && (( $shared_commits >= $min_shared_commits_to_report_pair ))
+    if (( $shared_commits >= $min_shared_commits_to_report_pair ))
     then    
-        echo $pair' - share' $shared_commits' commits - '$combined_commits_to_pair' affect this pair - '$ratio'% of times both files are changed'
+        echo $pair','$shared_commits',shared commits'
     fi
-
 done
-
-#todo: jak wysoko trzeba wyjść żeby znaleźć wspólnego parenta a/b/c.cs a/d/e.cs > 2
